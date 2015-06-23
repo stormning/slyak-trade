6eaa086f172cda73f63429624835da7c31c8c276
stormning <stormning@163.com>
2015-06-23 13:30:55 +0800

    menu cache

--- src/main/java/com/slyak/util/Context.java
+++ src/main/java/com/slyak/util/Context.java
@@ -2,17 +2,17 @@ package com.slyak.util;
 
 import com.alibaba.fastjson.JSON;
 import com.google.common.collect.Lists;
+import com.slyak.core.FileUtils;
 import com.slyak.core.freemarker.Ftm;
 import com.slyak.core.spring.web.AppContext;
-import org.apache.commons.io.FileUtils;
 import org.springframework.core.io.DefaultResourceLoader;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
-import org.springframework.util.AntPathMatcher;
 import org.springframework.util.CollectionUtils;
 import org.springframework.web.servlet.mvc.condition.RequestCondition;
 import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
@@ -30,20 +30,18 @@ public class Context {
 
     private static ResourceLoader resourceLoader = new DefaultResourceLoader();
 
-    private AntPathMatcher pathMatcher = new AntPathMatcher();
-
     private List<Menu> menus = null;
 
     public List<Menu> getMenus() {
-        if (menus == null || true) {
-            Resource resource = resourceLoader.getResource(MENU_FILE);
-            try {
-                menus = JSON.parseArray(FileUtils.readFileToString(resource.getFile(), "UTF-8"), Menu.class);
-            } catch (IOException e) {
-                menus = Collections.emptyList();
+        Resource resource = resourceLoader.getResource(MENU_FILE);
+        try {
+            File file = resource.getFile();
+            if (FileUtils.isModified(file)) {
+                menus = JSON.parseArray(FileUtils.readFileToString(file, "UTF-8"), Menu.class);
             }
+        } catch (IOException e) {
+            menus = Collections.emptyList();
         }
-
         if (CollectionUtils.isEmpty(menus)) {
             return menus;
         }
@@ -56,6 +54,7 @@ public class Context {
 
     /**
      * TODO 是否适用所有场景?
+     *
      * @param menus
      * @param rc
      * @return
